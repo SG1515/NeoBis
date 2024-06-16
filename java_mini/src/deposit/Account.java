795e4d5e9945d6f03593b1668955772daf3ef806
yerim8373 <yerim8373@naver.com>
2024-06-17 01:13:28 +0900

    feature : Account, Customer, Trade 생성

---
+++ java_mini/src/deposit/Account.java
@@ -0,0 +1,70 @@
+package deposit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+	private String accountNumber;
+	private long balance;
+	private List<Trade> trades;
+	private Country country;
+
+	public Account() {}
+
+	public Account(Country country) {
+		// this.accountNumber = accountNumber; -> 랜덤 숫자
+		this.balance = 0;
+		this.trades = new ArrayList<>();
+		this.country = country;
+	}
+
+	public void printTrades(){
+		for (Trade tr : trades) {
+			tr.printTrade();
+		}
+	}
+
+	public void printAccount(){
+		System.out.println("계좌번호 : " + accountNumber);
+		System.out.println("잔액 : " + balance);
+		// printTrades();
+		System.out.println("국가 : " + country);
+	}
+
+	public void deposit(long amount){
+		balance += amount;
+		trades.add(new Trade())
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+
+	public List<Trade> getTrades() {
+		return trades;
+	}
+
+	public void setTrades(List<Trade> trades) {
+		this.trades = trades;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+}
